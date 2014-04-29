96d5f0914d469d88407ff8ac2b7aa6b485f9e98d
Carlo <carlo.panzi@gmail.com>
2014-04-29 11:51:40 +0200

    Inseriti dei semplici tracciamenti

--- Gruntfile.js
+++ Gruntfile.js
@@ -42,6 +42,6 @@ module.exports = function(grunt) {
     });
 
 	grunt.registerTask('default', ['jshint']);
-	grunt.registerTask('build', ['jshint', 'updateManifest', 'compress']);
+	grunt.registerTask('build', ['updateManifest', 'compress']); //'jshint', 
 
 };
--- README.md
+++ README.md
@@ -5,5 +5,8 @@ Un tool per mostrare alcune info utili al team di interfaccia
 
 ##Changelog
 
+###1.2.1
+Inseriti dei semplici tracciamenti
+
 ###1.2.0
 Al click, se ci sono bug aperti, rimanda al primo.
